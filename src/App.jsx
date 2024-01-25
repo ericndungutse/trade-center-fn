319b4f553bd1e3da645d8fce30a52eeb0f75e7ff
ericndungutse <dav.ndungutse@gmil.com>
2024-01-25 13:19:00 +0200

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Home from './pages/Home';
 import RegisterCompany from './pages/RegisterCompany';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/register-company' element={<RegisterCompany />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+// src/components/Header.js
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='bg-gradient-to-r from-purple-700 via-purple-600 to-purple-700 text-white p-4'>
+      <div className='container mx-auto flex justify-between items-center'>
+        {/* Logo */}
+        <div className='flex items-center'>
+          <img
+            src='/path/to/logo.png'
+            alt='Logo'
+            className='h-10 w-10 mr-2 rounded-full'
+          />
+          <span className='text-lg font-bold'>Your Logo</span>
+        </div>
+
+        {/* Navigation */}
+        <nav className='space-x-4'>
+          <NavLink
+            to='/signin'
+            className='hover:text-gray-300 transition duration-300'
+          >
+            Sign In
+          </NavLink>
+          <NavLink
+            to='/signup'
+            className='hover:text-gray-300 transition duration-300'
+          >
+            Sign Up
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
